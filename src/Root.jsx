8bc62cbe871d6069bad610b5401fb52763c02f6b
Wyz3r <leonelislasglez@gmail.com>
2017-06-19 22:13:12 -0500

    icons corrects

--- src/Root.jsx
+++ src/Root.jsx
@@ -9,6 +9,9 @@ import AppBar from 'material-ui/AppBar'
 import NavigationClose from 'material-ui/svg-icons/navigation/close'
 import PersonAdd from 'material-ui/svg-icons/social/person-add'
 import School from 'material-ui/svg-icons/social/school'
+import Schedule from 'material-ui/svg-icons/action/schedule'
+import Info from 'material-ui/svg-icons/action/info'
+
 import Drawer from 'material-ui/Drawer'
 
 function handleTouchTap () {
@@ -40,10 +43,10 @@ class Root extends React.Component {
         <Paper className="menu-side" style={ style }>
           {/* <AppBar title={<span >Title</span>} onTitleTouchTap={handleTouchTap} /> */}
           <Menu style={menu}>
-            <Link to="/admin/Asistencia" ><MenuItem leftIcon={<PersonAdd />} primaryText="Asistencia" /></Link>
+            <Link to="/admin/Asistencia" ><MenuItem leftIcon={<Schedule />} primaryText="Asistencia" /></Link>
             <Link to="/admin/altaProf" ><MenuItem leftIcon={<PersonAdd />} primaryText="Alta de Profesores" /></Link>
             <Link to="/admin/altaAlum" ><MenuItem leftIcon={<School />} primaryText="Alta de Alumnos" /></Link>
-            <Link to="/admin/info" > <MenuItem leftIcon={<PersonAdd />} primaryText="Info" /></Link>
+            <Link to="/admin/info" > <MenuItem leftIcon={<Info />} primaryText="Info" /></Link>
           </Menu>
         </Paper>
         <Drawer
@@ -51,10 +54,10 @@ class Root extends React.Component {
           onRequestChange={(sideMenu) => this.setState({sideMenu})}
           open={sideMenu}>
           <AppBar title={<span >Menu</span>} onLeftIconButtonTouchTap={() => this.setState({ sideMenu: false })} />
-          <Link to="/admin/Asistencia" ><MenuItem leftIcon={<PersonAdd />} primaryText="Asistencia" /></Link>
+          <Link to="/admin/Asistencia" ><MenuItem leftIcon={<Schedule />} primaryText="Asistencia" /></Link>
           <Link to="/admin/altaProf" ><MenuItem leftIcon={<PersonAdd />} primaryText="Alta de Profesores" /></Link>
           <Link to="/admin/altaAlum" ><MenuItem leftIcon={<School />} primaryText="Alta de Alumnos" /></Link>
-          <Link to="/admin/info" > <MenuItem leftIcon={<PersonAdd />} primaryText="Info" /></Link>
+          <Link to="/admin/info" > <MenuItem leftIcon={<Info />} primaryText="Info" /></Link>
        </Drawer>
           { this.props.children }
         </div>
